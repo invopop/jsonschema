ed817e0b2c7064a79035679e8936e689b49c3c34
Sam Lown <sam.lown@invopop.com>
2023-09-06 15:06:11 +0000

    Fixing linting strategy

--- reflect_test.go
+++ reflect_test.go
@@ -34,13 +34,13 @@ type SomeBaseType struct {
 	// The jsonschema required tag is nonsensical for private and ignored properties.
 	// Their presence here tests that the fields *will not* be required in the output
 	// schema, even if they are tagged required.
-	somePrivateBaseProperty   string          `jsonschema:"required"`
+	somePrivateBaseProperty   string          `jsonschema:"required"` //nolint:unused
 	SomeIgnoredBaseProperty   string          `json:"-" jsonschema:"required"`
 	SomeSchemaIgnoredProperty string          `jsonschema:"-,required"`
 	Grandfather               GrandfatherType `json:"grand"`
 
 	SomeUntaggedBaseProperty           bool `jsonschema:"required"`
-	someUnexportedUntaggedBaseProperty bool
+	someUnexportedUntaggedBaseProperty bool //nolint:unused
 }
 
 type MapType map[string]interface{}
@@ -49,7 +49,7 @@ type ArrayType []string
 
 type nonExported struct {
 	PublicNonExported  int
-	privateNonExported int
+	privateNonExported int // nolint:unused
 }
 
 type ProtoEnum int32
