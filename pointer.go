28de468d515f8efd87378062566bb4e372cb57fb
candiduslynx <candiduslynx@gmail.com>
2023-10-03 21:03:25 +0300

    annotate

--- pointer.go
+++ pointer.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 )
 
+// JSONPointer escapes the path given to present a valid JSON pointer.
+// This is required when the path has special characters like `/` or `~`
 func JSONPointer(path string) string {
 	path = strings.ReplaceAll(path, "~", "~0")
 	path = strings.ReplaceAll(path, "/", "~1")
